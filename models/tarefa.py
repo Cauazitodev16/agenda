16c855a9a516cec8c99d00756fd7cf734e98bfed
Cauazitodev16 <rubislinscaua@gmail.com>
2026-02-09 16:18:52 -0300

    feat: adicionando funcionalidade e botões de excluir e editar tarefas

--- models/tarefa.py
+++ models/tarefa.py
@@ -3,8 +3,7 @@ from typing import Self, Any, Optional
 from sqlite3 import Cursor
 
 class Tarefa:
-    def __init__(self: Self, titulo_tarefa: Optional[str] = '', data_conclusao: Optional[str] = None, 
-    id_tarefa: Optional[int] = None) -> None:
+    def __init__(self: Self, titulo_tarefa: Optional[str], data_conclusao: Optional[str] = None, id_tarefa: Optional[int] = None) -> None:
         self.titulo_tarefa: Optional[str] = titulo_tarefa
         self.data_conclusao: Optional[str] = data_conclusao
         self.id_tarefa: Optional[int] = id_tarefa
@@ -19,7 +18,7 @@ class Tarefa:
         with Database('./data/tarefas.sqlite3') as db:
             query: str = 'SELECT titulo_tarefa, data_conclusao FROM tarefas WHERE id = ?;'
             params: tuple = (id,)
-            resultado = list[Any] = db.buscar_tudo(query, params) # pyright: ignore[reportGeneralTypeIssues]
+            resultado: list[Any] = db.buscar_tudo(query, params)
             # resultado = [["titulo_tarefa", "data_conclusao"]]
 
             # Desempacotamento de coleção
@@ -52,3 +51,12 @@ class Tarefa:
             params: tuple = (self.id_tarefa,)
             resultado: Cursor = db.executar(query, params)
             return resultado
+        
+    def atualizar_tarefa(self) -> Cursor:
+        with Database('./data/tarefas.sqlite3') as db:
+            query: str = 'UPDATE tarefas SET titulo_tarefa = ?, data_conclusao = ? WHERE id = ?;'
+            params: tuple = (self.titulo_tarefa, self.data_conclusao, self.id_tarefa)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
+       
+            
